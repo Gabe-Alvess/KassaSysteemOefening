0bcfd72f861916ca93abf1220ca020e2915d13d0
Biel-Alves <gabriel_vieira2018@hotmail.com>
2023-02-03 15:32:21 +0100

    update

--- src/be/intecbrussel/MainApp.java
+++ src/be/intecbrussel/MainApp.java
@@ -9,30 +9,42 @@ import java.util.Scanner;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("---- Oefening - KassaSysteem ----");
+        System.out.println(" ");
 
         Random random = new Random();
         Scanner myScanner = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("#.##");
-        StringBuilder producten = new StringBuilder("");
 
         double broodPrijs = random.nextDouble(1, 3);
         double melkPrijs = random.nextDouble(1, 5);
         double waterPrijs = random.nextDouble(1, 3);
         double chocoPrijs = random.nextDouble(1, 7);
+        double totalePrijs = 0;
 
         int stockBrood = 5;
         int stockMelk = 5;
         int stockWater = 5;
         int stockChoco = 5;
+        int totaleStock = stockBrood + stockMelk + stockWater + stockChoco;
+
+        int aantalBrood = 0;
+        int aantalMelk = 0;
+        int aantalWater = 0;
+        int aantalChoco = 0;
+
+        double totaalBrood = 0;
+        double totaalMelk = 0;
+        double totaalWater = 0;
+        double totaalChoco = 0;
 
 
         System.out.println("Producten: ");
-        System.out.println("brood: " + df.format(broodPrijs) + "€");
-        System.out.println("melk: " + df.format(melkPrijs) + "€");
-        System.out.println("water: " + df.format(waterPrijs) + "€");
-        System.out.println("choco: " + df.format(chocoPrijs) + "€");
+        System.out.println("Brood: " + df.format(broodPrijs) + "€");
+        System.out.println("Melk: " + df.format(melkPrijs) + "€");
+        System.out.println("Water: " + df.format(waterPrijs) + "€");
+        System.out.println("Choco: " + df.format(chocoPrijs) + "€");
+        System.out.println(" ");
 
-        double total = 0;
         int number = 0;
         boolean afrekenen = false;
 
@@ -43,47 +55,67 @@ public class MainApp {
                 System.out.println("Typ afrekenen als u klaar bent!!!");
             }
 
+            System.out.println(" ");
             String product = myScanner.nextLine();
+            product = product.toLowerCase();
+            System.out.println(" ");
+
             switch (product) {
                 case "brood":
                     stockBrood--;
+                    totaleStock--;
                     if (stockBrood >= 0) {
-                        total += broodPrijs;
+                        aantalBrood++;
+                        totaalBrood = broodPrijs * aantalBrood;
+                        totalePrijs += broodPrijs;
                         System.out.println("Brood was toegevoegd aan uw winkelwagen.");
+                    } else if (totaleStock <= 0) {
+                        System.out.println("Het spijt ons, we hebben geen producten meer in onze stock! :(");
                     } else {
                         System.out.println("Sorry, we hebben geen brood meer in onze stock!");
                     }
-                    producten.append(" brood -> " + df.format(broodPrijs) + "€ ->");
                     break;
                 case "melk":
                     stockMelk--;
+                    totaleStock--;
                     if (stockMelk >= 0) {
-                        total += melkPrijs;
+                        aantalMelk++;
+                        totaalMelk = melkPrijs * aantalMelk;
+                        totalePrijs += melkPrijs;
                         System.out.println("Melk was toegevoegd aan uw winkelwagen.");
+                    } else if (totaleStock <= 0) {
+                        System.out.println("Het spijt ons, we hebben geen producten meer in onze stock! :(");
                     } else {
                         System.out.println("Sorry, we hebben geen melk meer in onze stock!");
                     }
-                    producten.append(" melk -> " + df.format(melkPrijs) + "€ ->");
                     break;
                 case "water":
                     stockWater--;
+                    totaleStock--;
                     if (stockWater >= 0) {
-                        total += waterPrijs;
+                        aantalWater++;
+                        totaalWater = waterPrijs * aantalWater;
+                        totalePrijs += waterPrijs;
                         System.out.println("Water was toegevoegd aan uw winkelwagen.");
+                    } else if (totaleStock <= 0) {
+                        System.out.println("Het spijt ons, we hebben geen producten meer in onze stock! :(");
                     } else {
                         System.out.println("Sorry, we hebben geen water meer in onze stock!");
                     }
-                    producten.append(" water -> " + df.format(waterPrijs) + "€ ->");
                     break;
                 case "choco":
                     stockChoco--;
+                    totaleStock--;
                     if (stockChoco >= 0) {
-                        total += chocoPrijs;
+                        aantalChoco++;
+                        totaalChoco = chocoPrijs * aantalChoco;
+                        totalePrijs += chocoPrijs;
                         System.out.println("Choco was toegevoegd aan uw winkelwagen.");
+                    } else if (totaleStock <= 0) {
+                        System.out.println("Het spijt ons, we hebben geen producten meer in onze stock! :(");
                     } else {
                         System.out.println("Sorry, we hebben geen choco meer in onze stock!");
                     }
-                    producten.append(" choco -> " + df.format(chocoPrijs) + "€ ->");
                     break;
                 case "afrekenen":
                     afrekenen = true;
@@ -91,12 +123,26 @@ public class MainApp {
                 default:
                     System.out.println("We hebben dit product niet. Sorry!!!");
             }
+
         }
-        if (total == 0) {
-            System.out.println("Geen producten gekocht -> " + "Totale prijs = " + df.format(total) + "€.");
+        if (totalePrijs == 0) {
+            System.out.println("Geen producten gekocht!");
+            System.out.println(" ");
+            System.out.println("Het spijt ons dat we niet hebben wat je nodig had :(");
+            System.out.println("De volgende keer hebben we zeker alles wat je nodig zou hebben!");
+            System.out.println(" ");
+            System.out.println("Kom snel terug!");
         } else {
-            System.out.println("Producten:" + producten + " Totale prijs = " + df.format(total) + "€");
+            System.out.println("Producten:   Prijs:   Aantal:   Totaal: ");
+            System.out.println("  Brood      " + df.format(broodPrijs) + "€" + "      " + aantalBrood + "x" + "       " + df.format(totaalBrood) + "€");
+            System.out.println("  Melk       " + df.format(melkPrijs) + "€" + "      " + aantalMelk + "x" + "       " + df.format(totaalMelk) + "€");
+            System.out.println("  Water      " + df.format(waterPrijs) + "€" + "      " + aantalWater + "x" + "       " + df.format(totaalWater) + "€");
+            System.out.println("  Choco      " + df.format(chocoPrijs) + "€" + "      " + aantalChoco + "x" + "       " + df.format(totaalChoco) + "€");
+            System.out.println("-----------------------------------------");
+            System.out.println("Totale prijs = " + df.format(totalePrijs) + "€");
+            System.out.println(" ");
             System.out.println("Bedankt voor uw aankoop!");
+            System.out.println(" ");
             System.out.println("Kom snel terug!");
         }
     }
